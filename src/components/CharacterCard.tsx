ceefab4b41fbc610509761bd80ffea91314fc1aa
MARUNGs <mi1764@naver.com>
2024-10-27 11:39:40 +0900

    중간커밋

---
+++ src/components/CharacterCard.tsx
@@ -0,0 +1,7 @@
+export default function CharacterCard() {
+  return (
+    <>
+      <h1>캐릭터 카드입니다...</h1>
+    </>
+  );
+}
---
+++ src/styles/HomeStyled.ts
@@ -0,0 +1,43 @@
+import styled, { keyframes } from "styled-components";
+
+const fadeIn = keyframes`
+  from { opacity: 0;}
+  to { opacity: 1; }
+`;
+
+const fadeOut = keyframes`
+  from { opacity: 1; }
+  to { opacity: 0; }
+`;
+
+export const InButtonContainer = styled.div<{
+  $inButtonContainerHide: boolean;
+}>`
+  margin: 10% 10% auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  // hide
+  animation: ${({ $inButtonContainerHide }) =>
+    $inButtonContainerHide ? fadeOut : fadeIn};
+  transition-delay: ${({ $inButtonContainerHide }) =>
+    $inButtonContainerHide && `opacity 0.5s ease`};
+  visibility: ${({ $inButtonContainerHide }) =>
+    $inButtonContainerHide && `hidden`};
+  position: ${({ $inButtonContainerHide }) =>
+    $inButtonContainerHide && `absolute`};
+`;
+
+export const MainContainer = styled.div<{ $mainContainerShow: boolean }>`
+  top: 10%;
+  position: ${({ $mainContainerShow }) =>
+    $mainContainerShow ? "static" : "absolute"};
+  animation: ${({ $mainContainerShow }) =>
+      $mainContainerShow ? fadeIn : fadeOut}
+    0.5s ease-in-out;
+  opacity: ${({ $mainContainerShow }) => ($mainContainerShow ? 1 : 0)};
+  visibility: ${({ $mainContainerShow }) =>
+    $mainContainerShow ? "visible" : "hidden"};
+  transition: visibility 0s linear 0.5s; /* fadeOut 애니메이션 후에 visibility를 hidden으로 설정 */
+`;
